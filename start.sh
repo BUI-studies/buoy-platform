c2eb042d1f57ddf244db137b5b22ccdd4f8ec1fd
yamnyk <yamnyk.vlog@gmail.com>
2023-06-25 18:03:35 +0200

    change start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# Specify the path to the .env file
+ENV_FILE="./server/.env"
+
 # Load environment variables from .env file
-source ./server/.env
+if [[ -f "$ENV_FILE" ]]; then
+  set -a
+  source "$ENV_FILE"
+  set +a
+else
+  echo "Error: .env file not found"
+  exit 1
+fi
 
 # Start your application
 node ./server/dist/index.js
